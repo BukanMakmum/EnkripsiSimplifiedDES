1e8d79b72ae22d6b56155b9ae19f255698537b62
Imam <32379649+BukanMakmum@users.noreply.github.com>
2023-09-10 20:34:30 +0700

    Update Dekripsi.py

--- Dekripsi.py
+++ Dekripsi.py
@@ -192,7 +192,7 @@ copy_button.grid(row=4, column=0, columnspan=2, pady=(20, 10), sticky=tk.NSEW)
 copy_button.grid_remove()  # Menyembunyikan tombol "Copy Hasil" saat awal aplikasi
 
 # Menambahkan keterangan hak cipta di tengah bawah
-copyright_label = ttk.Label(window, text="© 2023 @BukanMakmum.", foreground="gray", cursor="hand2")
+copyright_label = ttk.Label(window, text="© 2023 BukanMakmum.", foreground="gray", cursor="hand2")
 copyright_label.grid(row=5, column=0, columnspan=2, pady=(0, 10), sticky=tk.NSEW)
 
 # Mengatur teks hak cipta menjadi rata tengah horizontal
